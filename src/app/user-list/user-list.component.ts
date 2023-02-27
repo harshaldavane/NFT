cbf4e17bb4757c874e6d10c6bf6fd5643a6de8a3
harshaldavane <sagardavane@gmail.com>
2023-02-27 11:19:41 +0530

    first commit

---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styles: [
+    '.color-code{color:red}'
+
+
+  ]
+})
+export class UserListComponent {
+
+}
